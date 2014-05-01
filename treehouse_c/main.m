3e36b0204f5f61bb5020e50932ebb1db711d3d8c
Jennifer K Waldner <jenniferkwaldner@gmail.com>
2014-05-01 16:58:13 -0400

    Initial Commit

---
+++ treehouse_c/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  treehouse_c
+//
+//  Created by Jennifer Waldner on 5/1/14.
+//  Copyright (c) 2014 Jennifer Waldner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
